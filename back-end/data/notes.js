c743a54d1b7bfb9d6399cbd9df8a083832f65a38
Tanya Susan Thomas <tanyasusan92@gmail.com>
2023-03-10 11:50:16 +0400

    Material ui, read data from notes, express js

---
+++ back-end/data/notes.js
@@ -0,0 +1,35 @@
+const notes =[
+  {
+    id: 1,
+    title: "Home",
+    noteList: [
+      { number: 1, title: "Shop for milk and bread" },
+      { number: 2, title: "Plan for DS event" },
+    ],
+  },
+  {
+    id: 2,
+    title: "School",
+    noteList: [
+      { number: 1, title: "Finish Mathletics" },
+      { number: 2, title: "Cut and paste assignment" },
+      { number: 3, title: "Inventor story"},
+    ],
+  },
+  {
+    id: 3,
+    title: "Medical",
+    noteList: [
+      { number: 1, title: "Check vaccine card" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Budget",
+    noteList: [
+      { number: 1, title: "Combine bills" },
+      { number: 2, title: "Add maid salary" },
+    ],
+  },
+];
+module.exports = notes;
--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,22 @@
 const express= require("express");
+const notes = require('./data/notes');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res)=> {
     res.send("API is running");
 });
 
-app.listen(5000, console.log("Server on 5000"));
+app.get("/api/notes", (req, res)=> {
+    res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res)=> {
+    const note= notes.find((n)=>n.id == req.params.id);
+    res.json(note);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(5000, console.log(`Server on ${PORT}`));
